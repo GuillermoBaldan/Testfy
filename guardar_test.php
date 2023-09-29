f17fe885f5906347d3fba51778b540500cb9eaab
GuillermoBaldan <guillermobaldan@gmail.com>
2023-09-29 03:24:18 +0200

    register.php modified

--- guardar_test.php
+++ guardar_test.php
@@ -1,11 +1,25 @@
 <?php
+// Iniciar la sesión (esto debe estar al principio de cada script que use sesiones)
+session_start();
+
+// Comprobar si el usuario ha iniciado sesión y si el ID del usuario no es nulo
+if (!isset($_SESSION["usuario_id"]) || $_SESSION["usuario_id"] === null) {
+    // Si el usuario no ha iniciado sesión o el ID del usuario es nulo, redirige a la página de inicio de sesión
+    header("Location: index.html");
+    exit();
+}
+
+// Resto del código para insertar el test en la base de datos (sin cambios)
+// ...
+
+
 // Conexión a la base de datos (reemplaza con tus propias credenciales)
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "TestfyDB"; // Asegúrate de que sea el nombre correcto de tu base de datos
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname); // Selecciona la base de datos
 
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
@@ -14,26 +28,69 @@ if ($conn->connect_error) {
 // Obtener datos del formulario
 $titulo = $_POST["titulo"];
 $descripcion = $_POST["descripcion"];
-$enunciadoPregunta = $_POST["enunciado_pregunta"];
-$enunciadoRespuesta = $_POST["enunciado_respuesta"];
+$enunciado_pregunta = $_POST["enunciado_pregunta"];
+$enunciado_respuesta = $_POST["enunciado_respuesta"];
+$usuario_id = $_SESSION["usuario_id"];
 
-// Consulta SQL para insertar los datos en la base de datos
-$sql = "INSERT INTO Tests (Titulo, Descripcion) VALUES ('$titulo', '$descripcion')";
-if ($conn->query($sql) === TRUE) {
-    // Obtenemos el ID del test recién creado
-    $testID = $conn->insert_id;
+// Preparar la consulta SQL para insertar el test en la base de datos
+$sql = "INSERT INTO Tests (Titulo, Descripcion, UsuarioID) VALUES (?, ?, ?)";
 
-    // Insertamos la pregunta y la respuesta asociadas al test
-    $sql2 = "INSERT INTO PreguntasRespuestas (EnunciadoPregunta, EnunciadoRespuesta, TestID) VALUES ('$enunciadoPregunta', '$enunciadoRespuesta', $testID)";
-    
-    if ($conn->query($sql2) === TRUE) {
-        echo "Test creado exitosamente. <a href='inicio.html'>Volver al Inicio</a>";
+$stmt = $conn->prepare($sql);
+
+// Comprobar si la preparación de la consulta fue exitosa
+if ($stmt) {
+    // Vincular los parámetros
+    $stmt->bind_param("ssi", $titulo, $descripcion, $usuario_id);
+
+    // Ejecutar la consulta para insertar el test
+   // Ejecutar la consulta para insertar el test
+if ($stmt->execute()) {
+    // Obtener el ID del test recién insertado
+    $test_id = $conn->insert_id;
+
+    if ($test_id !== null) {
+        // Preparar la consulta para insertar la pregunta y respuesta en la tabla PreguntasRespuestas
+        $sqlPregResp = "INSERT INTO PreguntasRespuestas (EnunciadoPregunta, EnunciadoRespuesta, TestID) VALUES (?, ?, ?)";
+        $stmtPregResp = $conn->prepare($sqlPregResp);
+
+        if ($stmtPregResp) {
+            // Vincular los parámetros
+            $stmtPregResp->bind_param("ssi", $enunciado_pregunta, $enunciado_respuesta, $test_id);
+
+            // Ejecutar la consulta para insertar la pregunta y respuesta
+            if ($stmtPregResp->execute()) {
+                // Test y preguntas/respuestas creados exitosamente, redirige a alguna página de éxito o a donde desees
+                header("Location: test_creado_exitosamente.html");
+                exit();
+            } else {
+                // Error al ejecutar la consulta de preguntas/respuestas, muestra un mensaje de error
+                echo "Error al crear el test. <a href='crear_test.html'>Regresar</a>";
+            }
+
+            // Cerrar la declaración de preguntas/respuestas
+            $stmtPregResp->close();
+        } else {
+            // Error en la preparación de la consulta de preguntas/respuestas, muestra un mensaje de error
+            echo "Error al preparar la consulta de preguntas/respuestas. <a href='crear_test.html'>Regresar</a>";
+        }
     } else {
-        echo "Error al insertar la pregunta y respuesta: " . $conn->error;
+        // El ID del test es nulo, muestra una notificación
+        echo "El ID del test es nulo. <a href='crear_test.html'>Regresar</a>";
     }
 } else {
-    echo "Error al crear el test: " . $conn->error;
+    // Error al ejecutar la consulta de test, muestra un mensaje de error
+    echo "Error al crear el test. <a href='crear_test.html'>Regresar</a>";
 }
 
+// ...
+
+    // Cerrar la declaración de test
+    $stmt->close();
+} else {
+    // Error en la preparación de la consulta de test, muestra un mensaje de error
+    echo "Error al preparar la consulta de test. <a href='crear_test.html'>Regresar</a>";
+}
+
+// Cerrar la conexión
 $conn->close();
 ?>
--- register.php
+++ register.php
@@ -1,35 +1,42 @@
 <?php
-// Conexión a MySQL sin seleccionar una base de datos específica
-$servername = "localhost";
-$username = "root";
-$password = "";
+// Verificar si se ha enviado el formulario de registro
+if (isset($_POST["registro"])) {
+    // Obtener los datos del formulario
+    $nombre = $_POST["username"];
+    $email = $_POST["email"];
+    $contrasena = $_POST["password"];
 
-$conn = new mysqli($servername, $username, $password);
+    // Conexión a la base de datos (reemplaza con tus propias credenciales)
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "TestfyDB"; // Asegúrate de que sea el nombre correcto de tu base de datos
 
-if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
-}
+    $conn = new mysqli($servername, $username, $password);
 
-// Crear la base de datos TestfyDB si no existe
-$sqlCreateDB = "CREATE DATABASE IF NOT EXISTS TestfyDB";
-if ($conn->query($sqlCreateDB) === TRUE) {
-    echo "Base de datos TestfyDB creada con éxito.<br>";
-    
-    // Cambiar a la base de datos TestfyDB
-    $conn->select_db("TestfyDB");
+    // Verificar la conexión a la base de datos
+    if ($conn->connect_error) {
+        die("Error de conexión: " . $conn->connect_error);
+    }
 
-    // Crear la tabla Usuarios
-    $sqlCreateUsuariosTable = "CREATE TABLE IF NOT EXISTS Usuarios (
-        ID INT AUTO_INCREMENT PRIMARY KEY,
-        Nombre VARCHAR(255) NOT NULL,
-        Contrasena VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL
-    )";
+    // Crear la base de datos TestfyDB si no existe
+    $sqlCreateDB = "CREATE DATABASE IF NOT EXISTS TestfyDB";
+    if ($conn->query($sqlCreateDB) === TRUE) {
+        echo "Base de datos TestfyDB creada con éxito.<br>";
 
-    if ($conn->query($sqlCreateUsuariosTable) === TRUE) {
-        echo "Tabla Usuarios creada con éxito.<br>";
-        
-        // Crear la tabla Tests
+        // Cambiar a la base de datos TestfyDB
+        $conn->select_db("TestfyDB");
+
+        // Verificar y crear la tabla Usuarios si no existe
+        $sqlCreateUsuariosTable = "CREATE TABLE IF NOT EXISTS Usuarios (
+            ID INT AUTO_INCREMENT PRIMARY KEY,
+            Nombre VARCHAR(255) NOT NULL,
+            Contrasena VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL
+        )";
+        $conn->query($sqlCreateUsuariosTable);
+
+        // Verificar y crear la tabla Tests si no existe
         $sqlCreateTestsTable = "CREATE TABLE IF NOT EXISTS Tests (
             ID INT AUTO_INCREMENT PRIMARY KEY,
             Titulo VARCHAR(255) NOT NULL,
@@ -38,39 +45,53 @@ if ($conn->query($sqlCreateDB) === TRUE) {
             UsuarioID INT,
             FOREIGN KEY (UsuarioID) REFERENCES Usuarios(ID)
         )";
+        $conn->query($sqlCreateTestsTable);
 
-        if ($conn->query($sqlCreateTestsTable) === TRUE) {
-            echo "Tabla Tests creada con éxito.<br>";
-            
-            // Crear la tabla PreguntasRespuestas
-            $sqlCreatePreguntasRespuestasTable = "CREATE TABLE IF NOT EXISTS PreguntasRespuestas (
-                ID INT AUTO_INCREMENT PRIMARY KEY,
-                EnunciadoPregunta TEXT,
-                EnunciadoRespuesta TEXT,
-                FechaUltimoAcierto TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                TiempoDelta INT,
-                TestID INT,
-                FOREIGN KEY (TestID) REFERENCES Tests(ID)
-            )";
+        // Verificar y crear la tabla PreguntasRespuestas si no existe
+        $sqlCreatePreguntasRespuestasTable = "CREATE TABLE IF NOT EXISTS PreguntasRespuestas (
+            ID INT AUTO_INCREMENT PRIMARY KEY,
+            EnunciadoPregunta TEXT,
+            EnunciadoRespuesta TEXT,
+            FechaUltimoAcierto TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            TiempoDelta INT,
+            TestID INT,
+            FOREIGN KEY (TestID) REFERENCES Tests(ID)
+        )";
+        $conn->query($sqlCreatePreguntasRespuestasTable);
 
-            if ($conn->query($sqlCreatePreguntasRespuestasTable) === TRUE) {
-                echo "Tabla PreguntasRespuestas creada con éxito.<br>";
-                
-                // Redirigir a inicio.html
+        // Hash de la contraseña para mayor seguridad (puedes usar otras funciones de hash)
+        $hashedPassword = password_hash($contrasena, PASSWORD_DEFAULT);
+
+        // Preparar la consulta SQL para insertar un nuevo usuario
+        $sqlInsertUsuario = "INSERT INTO Usuarios (Nombre, Contrasena, email) VALUES (?, ?, ?)";
+        $stmtInsertUsuario = $conn->prepare($sqlInsertUsuario);
+
+        // Comprobar si la preparación de la consulta fue exitosa
+        if ($stmtInsertUsuario) {
+            // Vincular los parámetros
+            $stmtInsertUsuario->bind_param("sss", $nombre, $hashedPassword, $email);
+
+            // Ejecutar la consulta para insertar el nuevo usuario
+            if ($stmtInsertUsuario->execute()) {
+                // Registro exitoso, redirige a alguna página de éxito o a donde desees
                 header("Location: inicio.html");
                 exit();
             } else {
-                echo "Error al crear la tabla PreguntasRespuestas: " . $conn->error;
+                // Error al ejecutar la consulta, muestra un mensaje de error
+                echo "Error al registrar el usuario. <a href='register.html'>Regresar</a>";
             }
+
+            // Cerrar la declaración
+            $stmtInsertUsuario->close();
         } else {
-            echo "Error al crear la tabla Tests: " . $conn->error;
+            // Error en la preparación de la consulta, muestra un mensaje de error
+            echo "Error al preparar la consulta. <a href='register.html'>Regresar</a>";
         }
     } else {
-        echo "Error al crear la tabla Usuarios: " . $conn->error;
+        echo "Error al crear la base de datos: " . $conn->error;
     }
-} else {
-    echo "Error al crear la base de datos: " . $conn->error;
-}
 
-$conn->close();
+    // Cerrar la conexión
+    $conn->close();
+}
 ?>
